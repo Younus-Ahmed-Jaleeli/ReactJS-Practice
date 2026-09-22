14da857d98a482afd342742b741ed9868b9e99d3
Younus-Ahmed-Jaleeli <younus.jaleeli2000@gmail.com>
2026-09-22 17:19:05 +0530

    ReactJS Day 0 Practice

---
+++ ReactJS Day 0 Practice/script.js
@@ -0,0 +1,11 @@
+// Fundamentals //
+
+console.log(React);
+
+// React Approach //
+
+const heading = React.createElement("h1",{id: "heading"},"Hello World from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
